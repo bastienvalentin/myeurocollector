6f9a1a528ec6ada5fc06c21ea5054260945b10fa
BASTIEN Valentin <bastienvalentin@gmail.com>
2017-08-28 13:54:17 +0200

    add util method to display square image

--- app/src/main/java/fr/vbastien/mycoincollector/util/SquareImageView.kt
+++ app/src/main/java/fr/vbastien/mycoincollector/util/SquareImageView.kt
@@ -1,7 +1,24 @@
 package fr.vbastien.mycoincollector.util
 
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+
 /**
  * Created by vbastien on 28/08/2017.
  */
-class SquareImageView {
+class SquareImageView : ImageView {
+
+    constructor(context : Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        if (measuredWidth > measuredHeight) {
+            setMeasuredDimension(measuredHeight, measuredHeight);
+        } else {
+            setMeasuredDimension(measuredWidth, measuredWidth);
+        }
+    }
 }
