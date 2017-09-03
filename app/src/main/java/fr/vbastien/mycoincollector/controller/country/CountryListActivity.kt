157f85151445170a9c6c503ebb74a23a977cccb9
BASTIEN Valentin <bastienvalentin@gmail.com>
2017-09-03 16:48:04 +0200

    resolve import

--- app/src/main/java/fr/vbastien/mycoincollector/controller/country/CountryListActivity.kt
+++ app/src/main/java/fr/vbastien/mycoincollector/controller/country/CountryListActivity.kt
@@ -15,7 +15,7 @@ import fr.vbastien.mycoincollector.db.Country
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_country.*
+import kotlinx.android.synthetic.main.activity_list_country.*
 
 class CountryListActivity : LifecycleActivity() {
 
